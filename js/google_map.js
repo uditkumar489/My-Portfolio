e333d4e82828e07d35aff188963e8653d15b5ecf
uditkumar489 <uditkumar489@gmail.com>
2017-10-19 14:13:02 +0530

    map api error resolved
    demo logitude & latitude applied !!

--- js/google_map.js
+++ js/google_map.js
@@ -5,13 +5,13 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(27.702491, 88.147535);
     // 39.399872
     // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 20,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
